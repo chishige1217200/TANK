38ef01b4e3904759228617e802acb5a4bd66e29c
chishige1217200 <daichicchi01@gmail.com>
2020-09-21 11:55:22 +0900

    [add] エフェクトに関するコメント

--- Assets/Scripts/GameScene/Bullet.cs
+++ Assets/Scripts/GameScene/Bullet.cs
@@ -18,11 +18,11 @@ public class Bullet : MonoBehaviour
         rend = new Renderer[5]; //配列の長さは5
         rb = GetComponent<Rigidbody>(); //Rigidbody情報の取得
         audioSource = GetComponents<AudioSource>();
-        rend[0] = GameObject.Find("BPS").GetComponent<Renderer>(); //Particle System情報の取得
-        rend[1] = GameObject.Find("BPS2").GetComponent<Renderer>();
-        rend[2] = GameObject.Find("BPS3").GetComponent<Renderer>();
-        rend[3] = GameObject.Find("Burst").GetComponent<Renderer>();
-        rend[4] = GameObject.Find("RPS").GetComponent<Renderer>();
+        rend[0] = GameObject.Find("BPS").GetComponent<Renderer>(); //Particle System情報の取得 通常弾エフェクト
+        rend[1] = GameObject.Find("BPS2").GetComponent<Renderer>(); //高速弾エフェクト
+        rend[2] = GameObject.Find("BPS3").GetComponent<Renderer>(); //反射弾エフェクト
+        rend[3] = GameObject.Find("Burst").GetComponent<Renderer>(); //
+        rend[4] = GameObject.Find("RPS").GetComponent<Renderer>(); //反射時エフェクト
 
         this.transform.rotation = Quaternion.Euler(0, -init_radian + 90f, 0); //回転処理（進行方向）
         init_radian = init_radian * (float)(Math.PI / 180); //ラジアン(pi)に変換
@@ -76,12 +76,14 @@ public class Bullet : MonoBehaviour
             Debug.Log("Hit!");
             //SoundEffect(1); //外部スクリプトによる命令に変更(消滅すると鳴らせない)
             Destroy(this.gameObject); //消滅
+            //発射タンク情報へのアクセス
         }
         if (collision.gameObject.tag == "Bullet") //弾同士で衝突した場合
         {
             Debug.Log("Bullet!");
             //衝突音？
             Destroy(this.gameObject);
+            //発射タンク情報へのアクセス
         }
         if (collision.gameObject.tag == "Mine" || collision.gameObject.tag == "Blast") //タンク・他の弾・地雷・爆風との接触
         {
