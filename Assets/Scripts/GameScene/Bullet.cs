688a0627f770ef8e37096e50020c92ae0bc68ade
chishige1217200 <daichicchi01@gmail.com>
2020-10-18 21:48:08 +0900

    [update] ほぼプレハブ化・初期化用関数実装

--- Assets/Scripts/GameScene/Bullet.cs
+++ Assets/Scripts/GameScene/Bullet.cs
@@ -3,46 +3,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject prefab; //跳弾エフェクトprefab情報を格納(Unity側で代入済み)
     private int ricochet = 10; //跳弾可能回数 要初期化
     private Rigidbody rb; //物理演算情報RigidBody
-    public GameObject prefab; //跳弾エフェクトprefab情報を格納
-    GameObject cloneObject; //跳弾エフェクトprefab情報をクローンして格納
+    private GameObject cloneObject; //跳弾エフェクトprefab情報をクローンして格納
+    private Renderer[] rend; //エフェクトレンダラー情報
 
     void Start()
     {
-        float force_x; //x方向の力
-        float force_z; //z方向の力
-        float forceIndex = 30f; //射出力基準値 要初期化 低速15f 高速30f
-        float init_radian = 30f; //初期進行角度 要初期化
-        Renderer[] rend; //エフェクトレンダラー情報
-
-        rend = new Renderer[5]; //配列の長さは5
-        rb = GetComponent<Rigidbody>(); //Rigidbody情報の取得
-        rend[0] = GameObject.Find("BPS").GetComponent<Renderer>(); //Particle System情報の取得 通常弾エフェクト
-        rend[1] = GameObject.Find("BPS2").GetComponent<Renderer>(); //高速弾エフェクト
-        rend[2] = GameObject.Find("BPS3").GetComponent<Renderer>(); //反射弾エフェクト
-        //rend[3] = GameObject.Find("RPS").GetComponent<Renderer>(); //反射時エフェクト(Prefabに移行)
-        //rend[4] = GameObject.Find("Burst").GetComponent<Renderer>(); //未定
-
-        this.transform.rotation = Quaternion.Euler(0, -init_radian + 90f, 0); //回転処理（進行方向）
-        init_radian = init_radian * (float)(Math.PI / 180); //ラジアン(pi)に変換
-        force_x = forceIndex * (float)Math.Cos(init_radian); //x軸方向の力を計算
-        force_z = forceIndex * (float)Math.Sin(init_radian); //z軸方向の力を計算
-        rb.AddForce(force_x, 0, force_z, ForceMode.VelocityChange); //瞬間的に弾に力を加える(質量無視)
-
-        if (forceIndex > 20f) //速度が20以上の弾エフェクト
-        {
-            rend[1].enabled = true;
-        }
-
-        if (ricochet > 2) //跳弾数が2以上の弾
-        {
-            rend[2].enabled = true;
-        }
-        else //跳弾数が1以下の弾
-        {
-            rend[0].enabled = true;
-        }
+        Shot(15f, 30f, 10); //Temporary
     }
 
     void OnCollisionEnter(Collision collision) //物体の衝突を見る関数(非貫通)
@@ -50,7 +19,6 @@ public class Bullet : MonoBehaviour
         float speed_x; //x軸方向移動速度
         float speed_z; //z軸方向移動速度
         float radian; //反射後の角度を計算
-
 
         if (collision.gameObject.tag == "Wall" || collision.gameObject.tag == "WeakWall") //壁と衝突した場合
         {
@@ -78,7 +46,7 @@ public class Bullet : MonoBehaviour
             Debug.Log("Hit!");
             //SoundEffect(1); //外部スクリプトによる命令に変更(消滅すると鳴らせない)
             Destroy(this.gameObject); //消滅
-            //発射タンク情報へのアクセス
+            //発射タンク情報へのアクセス(不要?)
         }
         if (collision.gameObject.tag == "Bullet") //弾同士で衝突した場合
         {
@@ -86,13 +54,39 @@ public class Bullet : MonoBehaviour
             cloneObject = Instantiate(prefab, this.transform.position, Quaternion.identity);
             //SoundEffect(1); //外部スクリプトによる命令に変更(消滅すると鳴らせない)
             Destroy(this.gameObject); //消滅
-            //発射タンク情報へのアクセス
+            //発射タンク情報へのアクセス(不要?)
         }
         if (collision.gameObject.tag == "Mine" || collision.gameObject.tag == "Blast") //タンク・他の弾・地雷・爆風との接触
         {
             Destroy(this.gameObject); //消滅
-            //発射タンク情報へのアクセス
+            //発射タンク情報へのアクセス(不要?)
         }
+    }
+    public void Shot(float forceIndex, float init_radian, int rico) //発射速度，発射角度，跳弾回数
+    {
+        float force_x; //x方向の力
+        float force_z; //z方向の力
+        //float forceIndex = 30f; //射出力基準値 要初期化 低速15f 高速30f
+        //float init_radian = 30f; //初期進行角度 要初期化
+
+        ricochet = rico; //反射回数の代入
+        rend = new Renderer[3]; //配列の長さは3
+        rb = GetComponent<Rigidbody>(); //Rigidbody情報の取得
+        rend[0] = GameObject.Find("BPS").GetComponent<Renderer>(); //Particle System情報の取得 通常弾エフェクト
+        rend[1] = GameObject.Find("BPS2").GetComponent<Renderer>(); //高速弾エフェクト
+        rend[2] = GameObject.Find("BPS3").GetComponent<Renderer>(); //反射弾エフェクト
+        //rend[3] = GameObject.Find("Burst").GetComponent<Renderer>(); //未定
+
+        this.transform.rotation = Quaternion.Euler(0, -init_radian + 90f, 0); //回転処理（進行方向）
+        init_radian = init_radian * (float)(Math.PI / 180); //ラジアン(pi)に変換
+        force_x = forceIndex * (float)Math.Cos(init_radian); //x軸方向の力を計算
+        force_z = forceIndex * (float)Math.Sin(init_radian); //z軸方向の力を計算
+        rb.AddForce(force_x, 0, force_z, ForceMode.VelocityChange); //瞬間的に弾に力を加える(質量無視)
+
+        if (forceIndex > 20f) rend[1].enabled = true; //速度が20以上の弾エフェクト
+
+        if (ricochet > 2) rend[2].enabled = true; //跳弾数が2以上の弾
+        else rend[0].enabled = true; //跳弾数が1以下の弾
     }
 }
 //Destroyは専用関数用意？
