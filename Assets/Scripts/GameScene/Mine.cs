a33dca8bfc4cca86cfef55b52c5d420e9848b54b
meromin007 <poiv7vtn@s.okayama-u.ac.jp>
2020-11-01 17:55:21 +0900

    [add] 地雷オブジェクトの設置

--- Assets/Scripts/GameScene/Mine.cs
+++ Assets/Scripts/GameScene/Mine.cs
@@ -1,15 +1,31 @@
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 
-public class Mine
-{
-    void Start()
-    {
+public class Mine : MonoBehaviour {
+    private Renderer rend;
+    void Start () {
+        //rend = GameObject.Find ("Big Explosion").GetComponent<Renderer> ();
+        Gobomb ();
+    }
+
+    void Update () {
 
     }
 
-    void Update()
-    {
+    async void Gobomb () {
+        Prediction ();
+        await Task.Delay (5000);
+        Explosion ();
+    }
+
+    void Prediction () {
+        Debug.Log ("Predict!");
 
     }
+
+    void Explosion () {
+        Debug.Log ("Bomb!");
+    }
+
 }
