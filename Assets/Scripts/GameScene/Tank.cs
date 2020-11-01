8140c00227da1a1b359c429f72ba0872356255e2
yuuki9089 <yuukiou90@gmail.com>
2020-11-01 17:34:03 +0900

    [add] マウス検出とタンク移動

--- Assets/Scripts/GameScene/Tank.cs
+++ Assets/Scripts/GameScene/Tank.cs
@@ -1,15 +1,19 @@
 using System;
 using UnityEngine;
 
-public class Tank
+public class Tank : MonoBehaviour
 {
+    private Transform bodyTransform;
+    private Transform batteryTransform;
+
     void Start()
     {
-
+        bodyTransform = GameObject.Find("Body").GetComponent<Transform>();
+        batteryTransform = GameObject.Find("Battery").GetComponent<Transform>();
     }
 
     void Update()
     {
-
+        this.transform.position += new Vector3(-0.01f, 0, 0);
     }
 }
