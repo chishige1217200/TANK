3493a176c26b8eb8edf52fca34783e4de25ab2fc
chishige1217200 <daichicchi01@gmail.com>
2020-11-01 21:07:44 +0900

    [add] コメント

--- Assets/Scripts/GameScene/Tank.cs
+++ Assets/Scripts/GameScene/Tank.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 public class Tank : MonoBehaviour
 {
-    private Transform bodyTransform;
-    private Transform batteryTransform;
+    private Transform bodyTransform; //BodyオブジェクトのTransformコンポーネント情報
+    private Transform batteryTransform; //BatteryオブジェクトのTransformコンポーネント情報
 
     void Start()
     {
-        bodyTransform = GameObject.Find("Body").GetComponent<Transform>();
-        batteryTransform = GameObject.Find("Battery").GetComponent<Transform>();
+        bodyTransform = GameObject.Find("Body").GetComponent<Transform>(); //BodyオブジェクトのTransformコンポーネント情報を取得
+        batteryTransform = GameObject.Find("Battery").GetComponent<Transform>(); //BatteryオブジェクトのTransformコンポーネント情報を取得
     }
 
     void Update()
     {
-        this.transform.position += new Vector3(-0.01f, 0, 0);
+        this.transform.position += new Vector3(-0.01f, 0, 0); //このスクリプトがアタッチされているオブジェクトのTransform.positionをx軸方向に-0.01ずつ移動する
     }
 }
