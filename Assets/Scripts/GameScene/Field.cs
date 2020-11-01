45e139183dd3f61d0173c2c25e406aaa8ddeef73
YuuYuu523 <pa5l4tp2@s.okayama-u.ac.jp>
2020-11-01 17:53:04 +0900

    [add] 壁プレハブと高さ変更

--- Assets/Scripts/GameScene/Field.cs
+++ Assets/Scripts/GameScene/Field.cs
@@ -1,15 +1,26 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Field
+public class Field : MonoBehaviour
 {
+    public GameObject insideWall;
+    private GameObject cloneObject;
+    private Transform cloneObjectTransform;
     void Start()
     {
-
+        insideObject(1,50,1);
     }
 
     void Update()
     {
 
+    }
+    void insideObject(int x, int y, int z)
+    {
+        //this.Transform.Scale(0,y,0);
+        cloneObject = Instantiate(insideWall, new Vector3(-5.0f*x-2.5f, y/2.0f, 5.0f*z+2.5f), Quaternion.identity);
+        cloneObjectTransform = cloneObject.GetComponent<Transform>();
+        cloneObjectTransform.localScale = new Vector3(5, y, 5);
     }
 }
