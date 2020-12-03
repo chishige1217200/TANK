d119307281fae60617e6bdd3a812a26ce3706d8e
chishige1217200 <daichicchi01@gmail.com>
2020-12-03 21:36:41 +0900

    [add] Wall

--- Assets/Scripts/GameScene/Field.cs
+++ Assets/Scripts/GameScene/Field.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Field : MonoBehaviour
 {
-    public GameObject insideWall; //Prefab情報を格納
+    public GameObject insideWall;   //Prefab情報を格納
+    public GameObject hole;         //Prefab情報を格納
     private GameObject cloneObject; //Instantiate関数により生成したインスタンス情報(GameObject型)を格納
     private Transform cloneObjectTransform; //cloneObjectのTransformコンポーネント情報を格納
     void Start()
     {
-        insideObject(1,10,1); //insideObject関数実行
+        insideObject(1, 10, 1); //insideObject関数実行
     }
 
     void Update()
@@ -18,7 +19,7 @@ public class Field : MonoBehaviour
     }
     void insideObject(int x, int y, int z)
     {
-        cloneObject = Instantiate(insideWall, new Vector3(5.0f*x+2.5f, y/2.0f, 5.0f*z+2.5f), Quaternion.identity); //insideWallを指定した座標に生成し，そのGameObject情報をcloneObjectに格納
+        cloneObject = Instantiate(insideWall, new Vector3(5.0f * x + 2.5f, y / 2.0f, 5.0f * z + 2.5f), Quaternion.identity); //insideWallを指定した座標に生成し，そのGameObject情報をcloneObjectに格納
         cloneObjectTransform = cloneObject.GetComponent<Transform>(); //cloneObjectのTransformコンポーネント情報をcloneObjectTransformに格納
         cloneObjectTransform.localScale = new Vector3(5, y, 5); //壁の高さを変更
     }
